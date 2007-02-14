d856754be9a770114223f80bede8c432d153a4e9
Chris Mear <chris@feedmechocolate.com>
2007-02-14 21:11:08 +0000

    Remove lower limit on username length
    git-svn-id: https://svn.odegy.net/webapps/bunnylove/trunk@1045 a18515e9-6cfd-0310-9624-afb4ebaee84e

--- lib/acts_as_authenticated.rb
+++ lib/acts_as_authenticated.rb
@@ -15,7 +15,6 @@ module ActiveRecord
           validates_presence_of :password_confirmation, :if => :password_required?
           validates_length_of :password, :within => 4..40, :if => :password_required?
           validates_confirmation_of :password, :if => :password_required?
-          validates_length_of :username, :within => 3..40
           validates_uniqueness_of :username, :case_sensitive => false
           before_save :encrypt_password
           
