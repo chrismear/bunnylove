7ca15f86cea953983e219d6f737eb9d5c64c5d43
Chris Mear <chris@feedmechocolate.com>
2007-12-28 17:54:37 +0000

    Formatting
    git-svn-id: https://svn.odegy.net/webapps/bunnylove/trunk@1251 a18515e9-6cfd-0310-9624-afb4ebaee84e

--- test/functional/bunny_sessions_controller_test.rb
+++ test/functional/bunny_sessions_controller_test.rb
@@ -19,15 +19,14 @@ class BunnySessionsControllerTest < Test::Unit::TestCase
     assert_response :redirect
     assert_redirected_to "/valentines"
   end
-
+  
   def test_should_fail_login_and_not_redirect
     post :create, :username => 'chrismear', :password => 'bad password'
     assert_nil session[:bunny]
     assert_response :success
     assert_template 'bunny_sessions/new'
   end
-
-
+  
   def test_should_logout
     login_as :bunny => :chrismear
     delete :destroy, :id => "current"
