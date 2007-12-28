706d85679d716ba93f74e4b233779bc10b9fb983
Chris Mear <chris@feedmechocolate.com>
2007-12-28 17:12:34 +0000

    Update deployment recipe for Capistrano 2
    git-svn-id: https://svn.odegy.net/webapps/bunnylove/trunk@1244 a18515e9-6cfd-0310-9624-afb4ebaee84e

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,67 +1,46 @@
-# This defines a deployment "recipe" that you can feed to capistrano
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
-
-# Mongrel cluster tasks
-require 'mongrel_cluster/recipes'
-
-
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
-
 set :application, "bunnylove"
-set :repository, "http://support.odegy.net/svn/webapps/bunnylove/trunk"
+set :repository, "http://mear.dyndns.org/svn/webapps/bunnylove/trunk"
 
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
+set :deploy_to "/var/www/bunnylove.org.uk/application"
 
 role :web, "toru.odegy.net"
 role :app, "toru.odegy.net"
 role :db,  "toru.odegy.net", :primary => true
 
-# =============================================================================
-# OPTIONAL VARIABLES
-# =============================================================================
-set :deploy_to, "/var/www/bunnylove.org.uk/application" # defaults to "/u/apps/#{application}"
-set :user, "chris"            # defaults to the currently logged in user
-# set :scm, :darcs               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
-
-set :use_sudo, false
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
+after "deploy:update", :link_config
 
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
-
-# =============================================================================
-# TASKS
-# =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
-
-desc "Link in production config and other stuff"
-task :after_update_code do
-  run <<-CMD
-    ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml 
-  CMD
+desc "Link in production database config"
+task :link_config do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 end
+
+namespace :deploy do
+  namespace :mongrel do
+    [ :stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the mongrel appserver"
+      task t do
+        #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
+        invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method 
+      end
+    end
+  end
+
+  desc "Custom restart task for mongrel cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.restart
+  end
+
+  desc "Custom start task for mongrel cluster"
+  task :start, :roles => :app do
+    deploy.mongrel.start
+  end
+
+  desc "Custom stop task for mongrel cluster"
+  task :stop, :roles => :app do
+    deploy.mongrel.stop
+  end
+
+end
+
