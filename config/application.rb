4bfc8c2550b1ddafab5c7d82190449f69a4712aa
Chris Mear <chris@feedmechocolate.com>
2013-11-01 01:31:28 +0000

    Add an application.rb.

---
+++ config/application.rb
@@ -0,0 +1,60 @@
+# Copyright 2013 Chris Mear
+# 
+# This file is part of Bunnylove.
+# 
+# Bunnylove is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Bunnylove is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with Bunnylove.  If not, see <http://www.gnu.org/licenses/>.
+
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+module Bunnylove
+  class Application < Rails::Application
+    config.autoload_paths += [config.root.join('lib')]
+    config.encoding = 'utf-8'
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  
+    # Add additional load paths for your own custom dirs
+    # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  
+    # Specify gems that this application depends on and have them installed with rake gems:install
+    # config.gem "bj"
+    # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+    # config.gem "sqlite3-ruby", :lib => "sqlite3"
+    # config.gem "aws-s3", :lib => "aws/s3"
+  
+    # Only load the plugins named here, in the order given (default is alphabetical).
+    # :all can be used as a placeholder for all plugins not explicitly named
+    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  
+    # Skip frameworks you're not going to use. To use Rails without a database,
+    # you must remove the Active Record framework.
+    # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  
+    # Activate observers that should always be running
+    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    config.time_zone = 'UTC'
+  
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    # config.i18n.default_locale = :de
+  end
+end
